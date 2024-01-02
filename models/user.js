d270708ca67231ca6056c06cef2abeb268ce0269
samohiani <ohianisammy2005@gmail.com>
2024-01-02 14:25:56 +0100

    Encapsulating Logic in Mongoose Models

--- models/user.js
+++ models/user.js
@@ -1,7 +1,9 @@
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
-const User = mongoose.model('User', mongoose.Schema({
+const userSchema = new mongoose.Schema({
     name:{
         type: String,
         required: true,
@@ -21,7 +23,14 @@ const User = mongoose.model('User', mongoose.Schema({
         minlength: 5,
         maxlength: 1024
     }
-}));
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({ _id: this._id }, config.get('jwtPrivateKey'));
+    return token;
+}
+
+const User = mongoose.model('User', userSchema);
 
 function validateUser(user) {
     const schema = Joi.object({
