a2ababad89d5da89a9c94766a25bd495c81e9eb2
samohiani <ohianisammy2005@gmail.com>
2024-01-02 13:40:19 +0100

    implemented a jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,11 @@
-const bcrypt = require('bcrypt');
+const config = require('config'); 
+const jwt = require('jsonwebtoken');
 const Joi = require('joi');
+const bcrypt = require('bcrypt');
 const _ = require('lodash');
-const { User} = require('../models/user')
+const { User } = require('../models/user')
 const mongoose = require('mongoose');
 const express = require('express');
-const { valid } = require('joi');
 const router = express.Router();
 
 router.post('/', async (req, res) =>{
@@ -17,7 +18,8 @@ router.post('/', async (req, res) =>{
     const validPassword = await bcrypt.compare(req.body.password, user.password);
     if (!validPassword) return res.status(400).send('Invalid email or password.');
 
-    res.send(true);
+    const token = jwt.sign({ _id: user._id }, config.get('jwtPrivateKey'));
+    res.send(token);
 });
 
 function validate(req) {
