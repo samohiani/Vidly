0a9d8678135846a67ab54e411409e98d2ec92666
samohiani <ohianisammy2005@gmail.com>
2024-01-02 15:04:17 +0100

    creation of middleware to return decoded token

--- routes/genres.js
+++ routes/genres.js
@@ -9,6 +9,8 @@ router.get('/', async (req,res) => {
 });
 
 router.post('/', async (req, res) =>{
+
+
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
