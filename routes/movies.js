0459dcdc217f3afae9cbcffbf6165c11252445c2
samohiani <ohianisammy2005@gmail.com>
2024-01-03 11:51:19 +0100

    protected with the pist routes with a middleware

--- routes/movies.js
+++ routes/movies.js
@@ -1,3 +1,4 @@
+const auth = require('../middleware/auth');
 const { Movie, validate } = require('../models/movie')
 const {Genre} = require('../models/genre')
 const mongoose = require('mongoose');
@@ -9,7 +10,7 @@ router.get('/', async (req,res) => {
     res.send(movies)
 });
 
-router.post('/', async (req, res) =>{
+router.post('/', auth, async (req, res) =>{
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
