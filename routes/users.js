99f70d884e51718b77be69f24524f416a6d558f9
samohiani <ohianisammy2005@gmail.com>
2024-01-03 12:01:53 +0100

    getting the current user

--- routes/users.js
+++ routes/users.js
@@ -8,7 +8,12 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) =>{
+router.get('/me', auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select('-password');
+    res.send(user);
+});
+
+router.post('/', async (req, res) =>{
     const { error } = validate(req.body);
     if(error) return res.status(400).send(error.details[0].message);
 
