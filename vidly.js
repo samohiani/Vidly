74f7d75792ea6b0623f41e7f2845dfa404535fb9
samohiani <ohianisammy2005@gmail.com>
2024-01-04 17:14:25 +0100

    creation of startup folder to simplify main index code

--- vidly.js
+++ vidly.js
@@ -1,19 +1,14 @@
 require('express-async-errors');
 const winston = require('winston');
 require('winston-mongodb');
-const error = require('./middleware/error')
 const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
-const mongoose = require('mongoose');
-const genres = require('./routes/genres');
-const customers = require('./routes/customers');
-const movies = require('./routes/movies');
-const rentals = require('./routes/rentals');
-const users = require('./routes/users');
-const auth = require('./routes/auth');
 const express = require('express');
 const app = express();
+
+require('./startup/routes')(app);
+require('./startup/database')(app);
 
 winston.handleExceptions(
     new winston.transports.File({ filename: 'uncaughtExceptions.log' }));
@@ -28,27 +23,10 @@ winston.add(new winston.transports.MongoDB({
     level: 'info'
 }));
 
-const p = Promise.reject(new Error('Something failed miserably!'));
-p.then(() => console.log('Done'));
-
 if (!config.get('jwtPrivateKey')) {
     console.log('FATAL ERROR: jwtPrivateKey is not defined.');
     process.exit(1);
 }
-
-mongoose.connect('mongodb://localhost/vidly')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDb...', err))
-
-app.use(express.json());
-app.use('/api/genres', genres);
-app.use('/api/customers', customers);
-app.use('/api/movies', movies);
-app.use('/api/rentals', rentals);
-app.use('/api/users', users);
-app.use('/api/auth', auth);
-
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
