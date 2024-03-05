46d6b7f18d28205f7aa81bc7259046ddcc51f504
samohiani <ohianisammy2005@gmail.com>
2024-03-05 21:59:19 +0100

    clear git cache

--- vidly.js
+++ vidly.js
@@ -8,6 +8,5 @@ require('./startup/db')();
 require('./startup/config')();
 require('./startup/validation')();
 require('./startup/prod')(app);
-
 const port = process.env.PORT || 3000;
 app.listen(port, () => winston.info(`Listening on port ${port}...`)); 
