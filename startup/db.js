04437218816e874b70f1910f44a3a025ab767de2
samohiani <ohianisammy2005@gmail.com>
2024-03-06 12:22:00 +0100

    updated mongodb

--- startup/db.js
+++ startup/db.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 module.exports = function () {
   mongoose
     .connect(
-      "mongodb+srv://ohianisammy2005:<dasakantimuse1>@cluster0.u6pdi1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+      "mongodb+srv://ohianisammy2005:<JuJID9al3hL4FQu0>@cluster0.u6pdi1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
     )
     .then(() => winston.info("Connected to MongoDB..."));
 };
