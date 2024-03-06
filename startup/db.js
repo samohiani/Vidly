b630af3fc30a23e60b5749fe63a8889139523eb1
samohiani <ohianisammy2005@gmail.com>
2024-03-06 11:46:14 +0100

    update to mongodb

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,10 @@
-const winston = require('winston');
-const mongoose = require('mongoose');
+const winston = require("winston");
+const mongoose = require("mongoose");
 
-module.exports = function() {
-    mongoose.connect('mongodb://localhost/vidly')
-    .then(() => winston.info('Connected to MongoDB...'));
-}
+module.exports = function () {
+  mongoose
+    .connect(
+      "mongodb+srv://ohianisammy2005:<dasakantimuse1>@cluster0.u6pdi1b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    )
+    .then(() => winston.info("Connected to MongoDB..."));
+};
