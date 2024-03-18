e3cc75e6d074e0d7b7feb834f12aa4179c642e64
samohiani <ohianisammy2005@gmail.com>
2024-03-18 17:54:56 +0100

    change in environment variable

--- models/user.js
+++ models/user.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const config = require('config');
+require("dotenv").config();
 const Joi = require('joi');
 const mongoose = require('mongoose');
 
@@ -27,7 +27,10 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.get('jwtPrivateKey'));
+    const token = jwt.sign(
+      { _id: this._id, isAdmin: this.isAdmin },
+      process.env.jwtPrivateKey
+    );
     return token;
 }
 
